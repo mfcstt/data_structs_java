ec638b0dbd02fcbcd450d6dd035e74ce63429fa7
Maria Fernanda Rizzo <126708422+mfcstt@users.noreply.github.com>
2024-03-18 15:35:13 +0000

    Add launch configuration for PilhaVetor

---
+++ Pilha/empilhar/src/pilhavetor/Pilha.java
@@ -0,0 +1,69 @@
+
+package pilhavetor;
+
+import java.util.Scanner;
+
+public class Pilha {
+
+    static Scanner teclado = new Scanner(System.in);
+    
+    static char[] pilha = new char[50]; 
+    static int top = -1;
+
+
+    
+    public static boolean estaCheia(){
+        return top == pilha.length - 1;
+    }
+
+    public static boolean estaVazia(){
+        return top == -1;
+    }
+
+
+    public static void empilhar(char pilha[]){
+            if (!estaCheia()){
+                System.out.println("Digite uma frase:");
+                //converte a frase para char
+                String frase = teclado.nextLine();
+                char[] fraseVetor = frase.toCharArray();
+                for (int i = 0; i < fraseVetor.length; i++) {
+                    pilha[++top] = fraseVetor[i];
+                }
+            }
+            else{
+                throw new RuntimeException("Pilha Cheia.");
+            }
+        }
+    
+    // public static String desempilha(){
+    //     char retorno = "";
+    //     if (!estaVazia()){
+    //         retorno = pilha[top];
+    //         pilha[top--] = (Character) null;
+    //     }
+    //     else{
+    //         throw new RuntimeException("Pilha vazia.");
+    //     }
+    //     return retorno;
+        
+    // }
+    
+    public static char consultaTop(){
+        if (!estaVazia()){
+            return pilha[top];
+        }
+        else{
+            throw new RuntimeException("Pilha vazia.");
+        }
+    }
+    
+    public static String avaliar(){
+        String aux = "";
+        for (char valor : pilha) {
+            aux += valor + " -> ";
+        }
+        return aux;
+    }
+    
+}
---
+++ Pilha/empilhar/src/pilhavetor/PilhaVetor.java
@@ -0,0 +1,14 @@
+
+package pilhavetor;
+
+
+public class PilhaVetor {
+    
+    public static void main(String[] args) {
+
+        
+        char[] pilha = new char[50]; 
+        Pilha.empilhar(pilha);
+    }
+    
+}
